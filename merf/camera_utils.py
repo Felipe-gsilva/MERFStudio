4c4be8b45c35f1ad4826c1872b92f6473d961ee9
Felipe-gsilva <felipe.gsilva@protonmail.com>
2026-03-02 11:18:34 -0300

    new rotation_matrix_between

--- merf/camera_utils.py
+++ merf/camera_utils.py
@@ -1,21 +1,18 @@
-from nerfstudio.cameras.camera_utils import focus_of_attention, rotation_matrix
-
+from nerfstudio.cameras.camera_utils import focus_of_attention, rotation_matrix_between
 import math
 from typing import  Literal,  Tuple
-
 import numpy as np
 import torch
 from jaxtyping import Float
 from torch import Tensor
-
 _EPS = np.finfo(float).eps * 4.0
+
 def auto_orient_and_center_poses(
     poses: Float[Tensor, "*num_poses 4 4"],
     method: Literal["pca", "up", "vertical", "none"] = "up",
     center_method: Literal["poses", "focus", "none"] = "poses",
 ) -> Tuple[Float[Tensor, "*num_poses 3 4"], Float[Tensor, "3 4"]]:
     """Orients and centers the poses. We provide two methods for orientation: pca and up.
-
     pca: Orient the poses so that the principal directions of the camera centers are aligned
         with the axes, Z corresponding to the smallest principal component.
         This method works well when all of the cameras are in the same plane, for example when
@@ -25,7 +22,6 @@ def auto_orient_and_center_poses(
     vertical: Orient the poses so that the Z 3D direction projects close to the
         y axis in images. This method works better if cameras are not all
         looking in the same 3D direction, which may happen in camera arrays or in LLFF.
-
     There are two centering methods:
     poses: The poses are centered around the origin.
     focus: The origin is set to the focus of attention of all cameras (the
@@ -42,18 +38,18 @@ def auto_orient_and_center_poses(
     """
 
     origins = poses[..., :3, 3]
-
     mean_origin = torch.mean(origins, dim=0)
     translation_diff = origins - mean_origin
 
     if center_method == "poses":
-
         translation = mean_origin
-        
+
     elif center_method == "focus":
         translation = focus_of_attention(poses, mean_origin)
+
     elif center_method == "none":
         translation = torch.zeros_like(mean_origin)
+
     else:
         raise ValueError(f"Unknown value for center_method: {center_method}")
 
@@ -68,12 +64,14 @@ def auto_orient_and_center_poses(
         oriented_poses = transform @ poses
 
         if oriented_poses.mean(dim=0)[2, 1] < 0:
+
             eigen = torch.eye(4)
             eigen[1:3] = -eigen[1:3]
             oriented_poses[:, 1:3] = -1 * oriented_poses[:, 1:3]
             transform = torch.cat([transform,torch.tensor([[0.,0.,0.,1.]])],dim=0)
             transform = eigen @ transform
             transform = transform[:-1,:]
+
     elif method in ("up", "vertical"):
         up = torch.mean(poses[:, :3, 1], dim=0)
         up = up / torch.linalg.norm(up)
@@ -96,11 +94,13 @@ def auto_orient_and_center_poses(
             # value, we project the "up" vector on the plane spanned by the two
             # best singular vectors. We could also just fallback to the "up"
             # solution.
+
             if S[1] > 0.17 * math.sqrt(poses.shape[0]):
                 # regular non-degenerate configuration
                 up_vertical = Vh[2, :]
                 # It may be pointing up or down. Use "up" to disambiguate the sign.
                 up = up_vertical if torch.dot(up_vertical, up) > 0 else -up_vertical
+
             else:
                 # Degenerate configuration: project "up" on the plane spanned by
                 # the last two right singular vectors (which are orthogonal to the
@@ -108,11 +108,9 @@ def auto_orient_and_center_poses(
                 # projecting.
                 up = up - Vh[0, :] * torch.dot(up, Vh[0, :])
                 # re-normalize
-                up = up / torch.linalg.norm(up)
 
-        rotation = rotation_matrix(up, torch.Tensor([0, 0, 1]))
-        transform = torch.cat([rotation, rotation @ -translation[..., None]], dim=-1)
-        oriented_poses = transform @ poses
+        rotation = rotation_matrix_between(up, torch.Tensor([0, 0, 1]))
+
     elif method == "none":
         transform = torch.eye(4)
         transform[:3, 3] = -translation
@@ -120,5 +118,4 @@ def auto_orient_and_center_poses(
         oriented_poses = transform @ poses
     else:
         raise ValueError(f"Unknown value for method: {method}")
-
     return oriented_poses, transform
